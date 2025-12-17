09c56976535ebe98a7307c2821b5872c71264b54
huynhthingocdao1102-hub <huynhthingocdao1102@gmail.com>
2025-12-17 23:06:53 +0900

    upL1

---
+++ circle-cylinder/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    static void main(String[] args) {
+        Circle circle = new Circle(3,"black");
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(3,"blue", 5);
+        System.out.println(cylinder);
+    }
+}
